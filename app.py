e21c312738c892cf73079bdff62e33a5c216d696
Manasvinitroshan <manas.comet.25@gmail.com>
2024-08-17 00:47:10 -0500

    saved files

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, render_template
+import pickle
 
 app = Flask(__name__)
 
 @app.route("/")
 def resume():
     return render_template('resume.html')
+
+@app.route("/pred",methods=["POST","GET"])
+
+def pred():
+    if request.method == 'POST':
+
 
 if __name__ == '__main__':
     app.run(debug=True)
